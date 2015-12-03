74f135f9546a79dfafd94f1d0649412e7c2b762a
hu jin <hujin@humatoMacBook-Pro.local>
2015-12-02 22:16:39 -0500

    i wanted hide sircle for the difference part and show up sircle after i click but i failed

--- js/main.js
+++ js/main.js
@@ -1,44 +1,25 @@
-// var pattern = ['R', 'Y'];
-// var num = 0;
 
-// $('.panel').on('click', function() {
 
-// 	// Does the panel you clicked match the
-// 	// current item in the sequence
 
-// 	var colour = $(this).attr('data-colour');
-
-// 	if ( colour == pattern[num] ) {
-// 		console.log("CORRECT!");
-		
-// 		//Short form of: num = num + 1;
-// 		num++;
-
-// 		if (num > pattern.length-1 ) {
-// 			console.log("YOU WIN!!!!1");
-// 		}
-// 	}
-// 	else {
-// 		console.log("Wrong :(");
-// 	}
-
-// });
-
- // var 
- // var num = 0;
- // var click = $( this).on('click',function() ;
 $(document).ready(function() {
-
-// $( ".spots" ).hide();
-	
-
 $('.spots li').on('click',function() {
+	if ($(".spots li").css("display")=="none"){
+	$('li').show();
+	}else{
+	$(this).hide();
+}
    		console.log ( "good job!" );	
    		$(this).off('click')
+   		// $(this).removeClass('hero');
+   		// $(this).addClass('shown');
 
+
+
+});
+	
    		});
 	
-});
+
 
 
 // if (num >1)
