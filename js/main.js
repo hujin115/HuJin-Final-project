b439532de1cb7c08ccde6bbec1311bc0a0a07046
hu jin <hujin@humatoMacBook-Pro.local>
2015-12-18 01:37:19 -0500

    After you helped to solve the clik issue I changed all the images to chirstmas theme images and i added snow plug in from google and made game ove pages and win game page

--- js/main.js
+++ js/main.js
@@ -12,33 +12,26 @@ $(document).ready(function() {
 	changelevel();
 
 
-/*
-	$('.hero1').on('click',function() {
-		$('.hero1').css({'opacity':'100'});
-});
-	$('.hero2').on('click',function() {
-		$('.hero2').css({'opacity':'100'});
-});
 
-	$('.hero3').on('click',function() {
-		$('.hero3').css({'opacity':'100'});
-});
-
-	$('.hero4').on('click',function() {
-		$('.hero4').css({'opacity':'100'});
-});*/
-	
 
 	$('.spots li').on('click',function() {
 		var pair = $(this).addClass('solved').attr('data-pair');
-		$('[data-pair='+pair+']').addClass('solved');
+		$('.level[data-level="'+level+'"] [data-pair='+pair+']').addClass('solved');
 		solved++;  //increase by 1
-
+if ( $('[data-pair='+pair+']').opacity == 0) {
+			$(this).css('opacity') = '1';
+		}
+		;
 		if (solved == 4) {
 			solved = 0;
 			level++;
+
+
+	
 			changelevel();
 		}
+
+		
 
 	});
 	
@@ -46,29 +39,34 @@ $(document).ready(function() {
 
 
 
-	// var hero = $('.hero1').on('click',function() ++ $('.hero2').on('click',function()++ $('.hero3').on('click',function()++ $('.hero4').on('click',function()
+	
 
-	// 	if (hero.css("opacity")=="100"){
-	// 		$('.1st').hide();
-	// 	}
-   var wrong = document.getElementById('wrong');
-   $('.level').on('click',function() {
-  		$(this).on('click');
+		// if (hero.css("opacity")=="100"){
+		// 	$('.1st').hide();
+		// }
+   // var wrong = document.getElementById('wrong');
+   // $('.container').on('click',function() {
+  	// 	$(this).on('click');
    		
-    		wrong.load();
-    		wrong.play();
+   //  		wrong.load();
+   //  		wrong.play();
 
-    });
+   //  });
 
 
-  var right = document.getElementById('right');
-   $('.hero1').on('click',function() {
+  var right = document.getElementById('right1');
+   $('.spots li').on('click',function() {
    		$(this).on('click');
    		$(this).off('click')
    		right.load();
    		right.play();
 
    });
+
+
+   // if ( right == play() ){
+   // 	 	 wrong.stop();
+   // }
 
 
 
@@ -78,6 +76,15 @@ $(document).ready(function() {
       var count = parseInt($(this).html());
       if (count !== 0) {
         $(this).html(count - 1);
+    }
+      if (count == 0) {
+        
+        $('.level[data-level]').css(({opacity:0}))
+        $('.gameover1').css(({opacity:1}))
+        $('.gameover2').css(({opacity:1}));
+
+
+        
       }
     });
   };
@@ -87,45 +94,11 @@ $(document).ready(function() {
 });
 
 
-	
-  // $('.hero1').on('click',function() {
-		// $('.right').click(function() {
+$(document).ready( function(){
+    $.fn.snow();
+});
 
-		// 	if ($('.right').paused == false) {
-		// 		$('.right').pause();
-			
-		// 	} else  {
-		// 		$('.right').load();
-		// 		$(this).play();
-		// 	}
-            
-  //       });
-
-
-
-	
-
-
-	//  if ($(".hero1").css("opacity")=="100"){
- // 	$('.hero2').css({'opacity':'0'});
- // 		$('.hero3').css({'opacity':'0'});
- // 		$('.hero4').css({'opacity':'0'});
-	//  }else{
- // 	$('.hero1').css({'opacity':'0'});
- //  }
- // 
-   		// console.log ( "good job!" );	
-   		// $(this).off('click')
-   		// $(this).removeClass('hero');
-   		// $(this).addClass('shown');
-
-
-
-
-// if (num >1)
-
-//  
-  
+(function($){$.fn.snow=function(options){var $flake=$('<div id="flake" />').css({'position':'absolute','top':'-50px'}).html('&#10052;'),documentHeight=$(document).height(),documentWidth=$(document).width(),defaults={minSize:10,maxSize:20,newOn:500,flakeColor:"#FFFFFF"},options=$.extend({},defaults,options);var interval=setInterval(function(){var startPositionLeft=Math.random()*documentWidth-100,startOpacity=0.5+Math.random(),sizeFlake=options.minSize+Math.random()*options.maxSize,endPositionTop=documentHeight-40,endPositionLeft=startPositionLeft-100+Math.random()*200,durationFall=documentHeight*10+Math.random()*5000;$flake.clone().appendTo('body').css({left:startPositionLeft,opacity:startOpacity,'font-size':sizeFlake,color:options.flakeColor}).animate({top:endPositionTop,left:endPositionLeft,opacity:0.2},durationFall,'linear',function(){$(this).remove()});},options.newOn);};})(jQuery);
 
 
 
